f1fe175f625c47356c6465ec745954a5704200a0
philip <philster36@hotmail.co.uk>
2024-11-11 22:26:18 +0000

    ack manager follows preferred paradigm

--- src/acknowledgement/manager.rs
+++ src/acknowledgement/manager.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, net::ToSocketAddrs, time::{Duration, Instant}};
+use std::{
+    collections::HashMap,
+    net::ToSocketAddrs,
+    time::{Duration, Instant},
+};
 
 use crate::packet::PacketDelivery;
 
@@ -26,33 +30,26 @@ impl AcknowledgementManager {
         }
     }
 
-    /// Adds a packet that requires an ack to the waiting list, if the packet does not receive an
-    /// ack it will be resent to its target
-    pub(crate) fn add_awaiting_ack_packet_if_needed<A>(
+    pub(crate) fn get_new_ack_num(&mut self) -> AckNumber {
+        self.last_ack += 1;
+
+        if self.last_ack >= ACK_RESET_LIMIT {
+            self.last_ack = 0;
+        }
+
+        self.last_ack
+    }
+
+    pub(crate) fn insert_packet_into_ack_waiting_list<A>(
         &mut self,
-        packet_delivery: PacketDelivery,
+        ack_num: AckNumber,
         buf: Vec<u8>,
         addr: A,
-    ) -> AckNumber
-    where
+    ) where
         A: ToSocketAddrs + Into<String>,
     {
-        if matches!(packet_delivery, PacketDelivery::Reliable)
-            || matches!(packet_delivery, PacketDelivery::ReliableSequenced)
-        {
-            if self.last_ack >= ACK_RESET_LIMIT {
-                self.last_ack = 0;
-            }
-
-            self.last_ack += 1;
-
-            self.packets_waiting_on_ack
-                .insert(self.last_ack, AckPacket::new(buf, Instant::now(), addr));
-
-            self.last_ack
-        } else {
-            0
-        }
+        self.packets_waiting_on_ack
+            .insert(ack_num, AckPacket::new(buf, Instant::now(), addr));
     }
 }
 
