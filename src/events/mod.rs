9cc52fb03e85214c18ded68ea0abc142be745706
philip <philster36@hotmail.co.uk>
2024-11-16 18:04:25 +0000

    polling event structure

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,27 +1,33 @@
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
+use crate::socket::{NautSocket, SocketType};
+
 /// The arguments that are passed into a callback, the sending address and the packet itself
-pub(crate) type CallbackArgs<'callback> = (SocketAddr, &'callback [u8]);
+pub(crate) type EventCallbackArgs<'callback> = (SocketAddr, &'callback [u8]);
 /// The structure of a callback, it passes in a reference to the socket type and the arguments
-pub(crate) type Callback<T> = dyn Fn(&T, CallbackArgs) + Send + Sync;
+pub(crate) type EventCallback<T> = dyn Fn(&T, EventCallbackArgs) + Send + Sync;
+/// The arguments that are passed into a polled callback
+pub(crate) type PolledCallback<T> = dyn Fn(&T) + Send + Sync;
 
 /// Listens to and emits events, running callbacks on events that have been emitted
-pub(crate) struct EventEmitter<T> {
-    pub event_callbacks: HashMap<String, Vec<Arc<Callback<T>>>>,
+pub(crate) struct EventEmitter<'socket, T> where T: SocketType<'socket> {
+    pub event_callbacks: HashMap<String, Vec<Arc<EventCallback<T>>>>,
+    pub polled_callbacks: Vec<Arc<PolledCallback<NautSocket<'socket, T>>>>,
 }
 
-impl<T> EventEmitter<T> {
+impl<'socket, T> EventEmitter<'socket, T> where T: SocketType<'socket> {
     /// Creates a new event emitter
     pub(crate) fn new() -> Self {
         Self {
             event_callbacks: HashMap::new(),
+            polled_callbacks: Vec::new(),
         }
     }
 
     /// Registers a callback to be run when an event is emitted
     pub(crate) fn register_event<F>(&mut self, event: &str, f: F)
     where
-        F: Fn(&T, CallbackArgs) + Send + Sync + 'static,
+        F: Fn(&T, EventCallbackArgs) + Send + Sync + 'static,
     {
         let event = event.to_string();
         if let Some(callbacks) = self.event_callbacks.get_mut(&event) {
@@ -33,7 +39,7 @@ impl<T> EventEmitter<T> {
     }
 
     /// Emits an event and fires all callbacks registered for that event
-    pub(crate) fn emit_event(&self, event: &str, value: &T, args: CallbackArgs) {
+    pub(crate) fn emit_event(&self, event: &str, value: &T, args: EventCallbackArgs) {
         let Some(callbacks) = self.event_callbacks.get(event) else {
             return;
         };
@@ -42,9 +48,25 @@ impl<T> EventEmitter<T> {
             callback(value, args)
         }
     }
+
+    pub(crate) fn register_poll_event<F>(&mut self, f: F)
+    where
+        F: Fn(&NautSocket<T>) + Send + Sync + 'static,
+    {
+        self.polled_callbacks.push(Arc::new(f));
+    }
+
+    pub(crate) fn emit_polled_events(&self, socket: &NautSocket<'socket, T>)
+    where
+        T: SocketType<'socket>,
+    {
+        for callback in self.polled_callbacks.iter() {
+            callback(socket)
+        }
+    }
 }
 
-impl<T> Default for EventEmitter<T> {
+impl<'socket, T> Default for EventEmitter<'socket, T> where T: SocketType<'socket> {
     fn default() -> Self {
         Self::new()
     }
