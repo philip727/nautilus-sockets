65eed7fe78f0e07b825c473530e7c479707c0d5b
philip <philster36@hotmail.co.uk>
2024-11-16 19:24:26 +0000

    fixed logger

--- src/plugins/logging.rs
+++ src/plugins/logging.rs
@@ -6,14 +6,14 @@ pub struct LoggingPlugin;
 
 impl SocketPlugin<'_, NautServer> for LoggingPlugin {
     fn register(&self, socket: &mut crate::prelude::NautSocket<'_, NautServer>) {
-        socket.on_poll(move |server| {
-            while let Some(event) = server.inner.iter_server_events().next() {
+        socket.on_poll(move |socket| {
+            for event in socket.server().iter_server_events() {
                 println!("[LOG][SERVER EVENT] {event:?}");
             }
         });
 
-        socket.on_poll(move |server| {
-            for event in server.socket_events.iter() {
+        socket.on_poll(move |socket| {
+            for event in socket.socket_events.iter() {
                 println!("[LOG][SOCKET EVENT] {event:?}");
             }
         });
