6294bf68c332718f831dce7946acd83ad177b73a
philip <philster36@hotmail.co.uk>
2024-11-17 11:49:16 +0000

    generic instead of impl

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -298,7 +298,10 @@ where
     }
 
     /// Registers a [plugin](crate::plugins::SocketPlugin)
-    pub fn register_plugin(&'socket mut self, plugin: impl SocketPlugin<'socket, S>) {
+    pub fn register_plugin<P>(&'socket mut self, plugin: P)
+    where
+        P: SocketPlugin<'socket, S>,
+    {
         plugin.register(self);
     }
 
